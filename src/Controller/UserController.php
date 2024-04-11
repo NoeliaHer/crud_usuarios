46c90d37cc82bd80ccf37c3591f441dbce1ba3eb
Noelia <noeherreros88@gmail.com>
2024-04-11 17:01:33 +0200

    Formulario registro y login

---
+++ src/Controller/UserController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
+use App\Form\UserType;
+class UserController extends AbstractController
+{
+
+    #[Route('/user', name: 'app_user')]
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+   
+}
